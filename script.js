6c95e60b798710588909d97a208ab22a5f952cde
faekhatami <faekhatami@gmail.com>
2024-07-16 18:37:35 +0330

    variables

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 console.log("Hello, world!");
 console.log("Welcome to Javascript!");
+
+let name = "Alice";
+let age = 30;
+let isStudent = true;
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is Student?", isStudent);
