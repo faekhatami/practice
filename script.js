e40d0aa9fd10f6e18362fd7f9df1e9793d3cb1e2
faekhatami <faekhatami@gmail.com>
2024-07-16 19:39:10 +0330

    conditional statements

--- script.js
+++ script.js
@@ -15,3 +15,21 @@ function addNumbers(num1, num2) {
 
 let result = addNumbers(5, 3);
 console.log("Result of adding numbers:", result);
+
+function getGrade(score) {
+  if (score >= 90) {
+    return "A";
+  } else if (score >= 80) {
+    return "B";
+  } else if (score >= 70) {
+    return "C";
+  } else if (score >= 60) {
+    return "D";
+  } else {
+    return "F";
+  }
+}
+
+console.log("Grade for 95:", getGrade(95));
+console.log("Grade for 82:", getGrade(82));
+console.log("Grade for 68:", getGrade(68));
